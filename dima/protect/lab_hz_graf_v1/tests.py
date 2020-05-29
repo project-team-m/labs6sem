91c1e2611c6ce1180be0bf6b6d8619c34abcaa5c
dekeyel <dekeyel@dekeyel.com>
2020-05-29 17:47:56 +0300

    add otchout lab7 kobak

--- dima/protect/lab_hz_graf_v1/tests.py
+++ dima/protect/lab_hz_graf_v1/tests.py
@@ -1,2 +1,15 @@
-for i in range(1000):
-    print(chr(i))
+
+from random import randint // вроде библиотека не импортируется
+var n = 1000000;
+
+var p = 0;
+double x, y;
+for (var i = 0; i < n; ++i){
+    x = rand.Next(1001) / 1000;
+    y = rand.Next(1001) / 1000;
+    if x * x + y * y < 1{
+        p++;
+    }
+}
+
+Console.WriteLine(p / n * 4);
